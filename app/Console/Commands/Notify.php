58e06faa1e30fd494712007e757a33b68bb2e5b4
Bassel-kh <bassel.kh.svu@gmail.com>
2020-07-26 10:58:17 -0700

    seconds commit

---
+++ app/Console/Commands/Notify.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\NotifyEmail;
+use App\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class Notify extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'notify:email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'send email notify for all users every Day';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+       // $user = User::select('email') -> get();
+        //or
+        $emails = User::pluck('email') -> toArray();
+
+        foreach ($emails as $email){
+            // how to send emails in laravel
+            $data = ['title' => 'Programming', 'body' => 'php'];
+            Mail::to($email) -> send(new NotifyEmail($data));
+
+        }
+    }
+}
---
+++ app/Http/Controllers/SocialController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialController extends Controller
+{
+    public function redirect($service)
+    {
+        return Socialite::driver($service)->redirect();
+    }
+
+//    public function callback($service)
+//    {
+////        return $user = Socialite::with($service)-> user() ;
+//////        return view('welcome');
+//    }
+
+    public function callback($service,Request $request)
+    {
+        $user = Socialite::driver($service) ->stateless()-> user() ;
+        return response() -> json($user);
+    }
+}
